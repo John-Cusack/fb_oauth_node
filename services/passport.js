d066d174b10126d0a10ff819e816ce9cadc8bf01
john <johncusackccm@gmail.com>
2018-05-17 08:49:07 -0400

    files

---
+++ services/passport.js
@@ -0,0 +1,53 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({googleId: profile.id}).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({googleId: profile.id})
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.FACEBOOK_APP_ID,
+      clientSecret: keys.FACEBOOK_APP_SECRET,
+      callbackURL: '/auth/facebook/callback'
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({facebookId: profile.id}, function(err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
