decad1df9ab3665461a0b1fcfe7565d65f59e936
John-Cusack <johncusackccm@gmail.com>
2018-05-21 22:03:08 -0400

    merging dup accounts

--- services/passport.js
+++ services/passport.js
@@ -24,34 +24,27 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({
-            googleId: profile.id,
-            googleEmail: profile.emails[0].value
-          })
-            .save()
-            .then(user => done(null, user));
+      User.findOne({ facebookEmail: profile.emails[0].value }).then(
+        existingUser => {
+          if (existingUser) {
+            User.findOneAndUpdate(
+              { facebookEmail: profile.emails[0].value },
+              { googleId: profile.id },
+              { new: true }
+            ).then(done(null, existingUser));
+          } else {
+            new User({
+              googleId: profile.id,
+              googleEmail: profile.emails[0].value
+            })
+              .save()
+              .then(user => done(null, user));
+          }
         }
-      });
+      );
     }
   )
 );
-
-// passport.use(
-//   new FacebookStrategy(
-//     {
-//       clientID: keys.FACEBOOK_APP_ID,
-//       clientSecret: keys.FACEBOOK_APP_SECRET,
-//       callbackURL: '/auth/facebook/callback'
-//     },
-//     (accessToken, refreshToken, profile, done) => {
-//       console.log(profile);
-//     }
-//   )
-// );
 
 passport.use(
   new FacebookStrategy(
@@ -62,18 +55,51 @@ passport.use(
       profileFields: ['id', 'email', 'name']
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ facebookId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({
-            facebookId: profile.id,
-            facebookEmail: profile.emails[0].value
-          })
-            .save()
-            .then(user => done(null, user));
+      User.findOne({ googleEmail: profile.emails[0].value }).then(
+        existingUser => {
+          if (existingUser) {
+            User.findOneAndUpdate(
+              { googleEmail: profile.emails[0].value },
+              { facebookId: profile.id },
+              { new: true }
+            ).then(done(null, existingUser));
+          } else {
+            new User({
+              facebookId: profile.id,
+              facebookEmail: profile.emails[0].value
+            })
+              .save()
+              .then(user => done(null, user));
+          }
         }
-      });
+      );
     }
   )
 );
+
+// passport.use(
+//   new FacebookStrategy(
+//     {
+//       clientID: keys.FACEBOOK_APP_ID,
+//       clientSecret: keys.FACEBOOK_APP_SECRET,
+//       callbackURL: '/auth/facebook/callback',
+//       profileFields: ['id', 'email', 'name']
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       User.findOne({ googleEmail: profile.emails[0].value }).then(
+//         existingUser => {
+//           if (existingUser) {
+//             done(null, existingUser);
+//           } else {
+//             new User({
+//               facebookId: profile.id,
+//               facebookEmail: profile.emails[0].value
+//             })
+//               .save()
+//               .then(user => done(null, user));
+//           }
+//         }
+//       );
+//     }
+//   )
+// );
