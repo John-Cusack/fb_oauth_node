33c5151d32759e4090d16ae05d0e0e5ecc8b2b98
John-Cusack <johncusackccm@gmail.com>
2018-05-25 22:29:02 -0400

    s3 more

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,1 +1,24 @@
-module.export = app => {};
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+const requireLogin = require('../middlewares/requireLogin');
+const keys = require('../config/key');
+
+const s3 = new AWS.s3({
+  accessKeyId: keys.AWS_ACCESS,
+  secretAccessKey: keys.AWS_SECRET
+});
+
+module.export = app => {
+  app.get('/api/upload', requireLogin, (req, res) => {
+    const key = `${req.user.id}/${uuid()}.jpg`;
+    s3.getSignedUrl(
+      'putObject',
+      {
+        Bucket: 's3-video-bucket',
+        ContentType: 'jpeg',
+        Key: key
+      },
+      (err, url) => res.send({ key, url })
+    );
+  });
+};
